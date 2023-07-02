e4bdfa3b5aa862d1853c7a4dd754d2cc5bde0a90
karribalu <karri.balu123456@gmail.com>
2023-07-03 00:23:08 +0100

    update the code

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import { linkedin, github, twitter, mail } from "../../icons";
 import "./Main.css";
 import some from "../../assets/some.svg";
+
 const Main = ({mode}) =>{ 
     return(
         <div>
@@ -10,6 +11,7 @@ const Main = ({mode}) =>{
                     <span className="span-name">I'm Bala Subramanyam</span>
                     <span className="span-description">Passionate full stack developer experienced in React, Java spring boot and exploring Web3 Space actively and currently working in JPMC, Glasgow</span>
                     <div className={mode? "social-links-light" : "social-links-dark"}>
+                       
                         <img src={linkedin} alt="linkedin" className="social-link"/>
                         <img src={github} alt="github" className="social-link"/>
                         <img src={twitter} alt="twitter" className="social-link"/>
